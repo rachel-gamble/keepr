8a4a566fd54a318ad7468d33f1c2f337bb651c48
rachel-gamble <sautrah@gmail.com>
2023-01-27 12:37:38 -0700

    made models backend

--- keepr/Models/Account.cs
+++ keepr/Models/Account.cs
@@ -2,8 +2,9 @@ namespace keepr.Models;
 
 public class Account
 {
-  public string Id { get; set; }
-  public string Name { get; set; }
-  public string Email { get; set; }
-  public string Picture { get; set; }
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Picture { get; set; }
+    public string CoverImg { get; set; }
 }
