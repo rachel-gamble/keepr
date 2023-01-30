27a6c8779d7cfabe50b17c2328e033260ce3179c
rachel-gamble <sautrah@gmail.com>
2023-01-30 14:04:56 -0700

    keep services written out

--- keepr.client/src/services/KeepsService.js
+++ keepr.client/src/services/KeepsService.js
@@ -1,8 +1,37 @@
+import { AppState } from "../AppState";
+
 class KeepsService {
-    async create(formData){
+    async create(formData) {
         const res = await api.post("api/keeps", formData)
         AppState.keeps.unshift(res.data)
     }
+    async getAllKeeps() {
+        const res = await api.get("api/keeps")
+        AppState.keeps = res.data;
+        AppState.keeps.reverse()
+    }
+    async getUserKeeps(profileId) {
+        const res = await api.get("api/profiles/" + profileId + "/keeps")
+        AppState.keeps = res.data;
+        AppState.keeps.reverse()
+    }
+
+    async incrementViews() {
+        const keep = AppState.activeKeep
+        const id = keep.id
+        keep.views++
+        const res = await api.put("api/keeps/" + id, keep)
+        AppState.activeKeep = res.data
+        AppState.activeKeep.vaultKeepId = keep.vaultKeepId
+    }
+
+    async deleteKeep() {
+        const id = AppState.activeKeep.id
+        await api.delete("api/keeps/" + id)
+        const index = AppState.keeps.find(k => k.id == id)
+        AppState.keeps.splice(index, 1)
+    }
+
 }
 
 export const keepsService = new KeepsService()
