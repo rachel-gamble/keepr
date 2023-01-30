0f0a1ca27be16d02b7b82af2b9bf468af22e7d70
rachel-gamble <sautrah@gmail.com>
2023-01-30 13:59:41 -0700

    filled out AppState

--- keepr.client/src/AppState.js
+++ keepr.client/src/AppState.js
@@ -6,5 +6,13 @@ export const AppState = reactive({
   /** @type {import('./models/Account.js').Account} */
   account: {},
   keeps: [],
+  myVaults: [],
+  userKeeps: [],
+  vaultKeeps: [],
+  userVaults: [],
+  activeKeep: null,
+  activeVault: null,
+  activeProfile: null,
+  profiles: [],
 
 })
