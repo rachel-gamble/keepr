2972cd5af06cc1a7e440f9188584f9ce8946af01
rachel-gamble <sautrah@gmail.com>
2023-01-31 11:13:53 -0700

    wrote vault service functions

---
+++ keepr.client/src/services/VaultsService.js
@@ -0,0 +1,32 @@
+import { AppState } from "../AppState";
+import { logger } from "../utils/Logger";
+import { api } from "./AxiosService";
+
+class VaultsService {
+
+    async create(formData) {
+        formData.creatorId = AppState.account.id
+        const res = await api.post("api/vaults", formData)
+        AppState.userVaults.unshift(res.data);
+        AppState.myVaults.unshift(res.data);
+    }
+
+    async getById(vaultId) {
+        const res = await api.get("api/vaults/" + vaultId)
+        AppState.activeVault = res.data;
+    }
+
+    async getUserVaults(profileId) {
+        const res = await api.get("api/profiles/" + profileId + "/vaults")
+        AppState.userVaults = res.data;
+        AppState.userVaults.reverse();
+    }
+
+    async removeVault(vaultId) {
+        await api.delete("api/vaults/" + vaultId)
+        const index = AppState.myVaults.find(v => v.id == vaultId)
+        AppState.myVaults.splice(index, 1)
+    }
+}
+
+export const vaultsService = new VaultsService();
